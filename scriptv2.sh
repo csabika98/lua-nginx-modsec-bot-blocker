1b3810fc7166d01dff7b26f5ed27d6e68fc54055
csabika98 <sallcsa.csaba8@gmail.com>
2025-03-15 20:15:53 +0100

    Major rework (Dockerfile)

--- scriptv2.sh
+++ scriptv2.sh
@@ -41,6 +41,23 @@ export DEBIAN_FRONTEND=noninteractive
     download_tgz "https://luarocks.org/releases/luarocks-${VER_LUAROCKS}.tar.gz"
     download_zip "https://github.com/openresty/luajit2/archive/refs/heads/v2.1-agentzh.zip" luajit-2.1
     download_zip "https://github.com/vision5/ngx_devel_kit/archive/refs/heads/master.zip" ngx_devel_kit
+    download_zip "https://github.com/openresty/lua-nginx-module/archive/refs/heads/master.zip" lua-nginx-module
+    download_zip "https://github.com/openresty/headers-more-nginx-module/archive/refs/heads/master.zip" headers-more-nginx-module
+    download_zip "https://github.com/openresty/lua-upstream-nginx-module/archive/refs/heads/master.zip" lua-upstream-nginx-module
+    download_zip "https://github.com/leev/ngx_http_geoip2_module/archive/refs/heads/master.zip" ngx_http_geoip2_module
+    download_zip "https://github.com/nginx/njs/archive/refs/heads/master.zip" njs
+    download_zip "https://github.com/openresty/set-misc-nginx-module/archive/refs/heads/master.zip" set-misc-nginx-module
+    download_zip "https://github.com/openresty/stream-lua-nginx-module/archive/refs/heads/master.zip" stream-lua-nginx-module
+    download_zip "https://github.com/openresty/memc-nginx-module/archive/refs/heads/master.zip" memc-nginx-module
+    download_zip "https://github.com/openresty/xss-nginx-module/archive/refs/heads/master.zip" xss-nginx-module
+    download_zip "https://github.com/openresty/lua-ssl-nginx-module/archive/refs/heads/master.zip" lua-ssl-nginx-module
+    download_zip "https://github.com/openresty/encrypted-session-nginx-module/archive/refs/heads/master.zip" encrypted-session-nginx-module
+    download_zip "https://github.com/openresty/echo-nginx-module/archive/refs/heads/master.zip" echo-nginx-module
+    download_zip "https://github.com/knyar/nginx-lua-prometheus/archive/refs/heads/main.zip" nginx-lua-prometheus
+    download_zip "https://github.com/cloudflare/lua-upstream-cache-nginx-module/archive/refs/heads/master.zip" lua-upstream-cache-nginx-module
+    git clone --depth 1 https://github.com/owasp-modsecurity/ModSecurity-nginx
+    git clone --depth 1 https://github.com/owasp-modsecurity/ModSecurity
+    git clone --depth 1 --shallow-submodules https://github.com/coreruleset/coreruleset /etc/nginx/coreruleset
 
     # Build LuaJIT
     make -C luajit-2.1 -j$(nproc)
@@ -84,10 +101,45 @@ export DEBIAN_FRONTEND=noninteractive
         --with-compat \
         --with-file-aio \
         --with-threads \
-        --add-module=../ngx_devel_kit \
-        --add-module=../lua-nginx-module \
-        # Add remaining configure options...
-
+        --with-http_addition_module \
+        --with-http_auth_request_module \
+        --with-http_dav_module \
+        --with-http_flv_module \
+        --with-http_gunzip_module \
+        --with-http_gzip_static_module \
+        --with-http_mp4_module \
+        --with-http_random_index_module \
+        --with-http_realip_module \
+        --with-http_secure_link_module \
+        --with-http_slice_module \
+        --with-http_ssl_module \
+        --with-http_stub_status_module \
+        --with-http_sub_module \
+        --with-http_v2_module \
+        --with-mail \
+        --with-mail_ssl_module \
+        --with-stream \
+        --with-stream_realip_module \
+        --with-stream_ssl_module \
+        --with-stream_ssl_preread_module \
+        --with-ld-opt="-Wl,-rpath,${LUAJIT_LIB}" \
+        --with-cc-opt="-I${LUAJIT_INC}" \
+            $(echo \
+            --add-module=../ngx_devel_kit \
+            --add-module=../lua-nginx-module \
+            --add-module=../headers-more-nginx-module \
+            --add-module=../lua-upstream-nginx-module \
+            --add-module=../ngx_http_geoip2_module \
+            --add-module=../njs/nginx \
+            --add-module=../set-misc-nginx-module \
+            --add-module=../stream-lua-nginx-module \
+            --add-module=../memc-nginx-module \
+            --add-module=../xss-nginx-module \
+            --add-module=../lua-ssl-nginx-module \
+            --add-module=../encrypted-session-nginx-module \
+            --add-module=../echo-nginx-module \
+            --add-module=../lua-upstream-cache-nginx-module \
+            --add-module=../ModSecurity-nginx) && \
     make -j$(nproc)
     make install
     cd ..
@@ -99,19 +151,59 @@ export DEBIAN_FRONTEND=noninteractive
     make install
     cd ..
 
-    # Install Lua components
-    lua_components=" \
-        openresty/lua-resty-core:lib/resty/ \
-        # Add other components...
-    "
+    VER_LUA=5.1 \
+    LUA_LIB_DIR=/usr/local/share/lua/${VER_LUA} \
+    PREFIX=/usr/local
 
-    for component in ${lua_components}; do
-        repo=$(echo "${component}" | cut -d: -f1)
-        path=$(echo "${component}" | cut -d: -f2)
-        git clone --depth 1 "https://github.com/${repo}"
-        cp -R "${repo#*/}/${path}" "/usr/local/share/lua/${VER_LUA}"
-        rm -rf "${repo#*/}"
-    done
+    lua_components=" \
+        openresty/lua-resty-core \
+        openresty/lua-resty-lrucache \
+        openresty/lua-resty-dns \
+        openresty/lua-resty-redis \
+        openresty/lua-resty-mysql \
+        openresty/lua-resty-memcached \
+        openresty/lua-resty-shell \
+        openresty/lua-resty-signal \
+        openresty/lua-resty-websocket \
+        openresty/lua-tablepool \
+        openresty/lua-resty-string \
+        openresty/lua-resty-upload \
+        openresty/lua-resty-upstream-healthcheck \
+        openresty/lua-resty-balancer \
+        cloudflare/lua-resty-logger-socket:lib \
+        knyar/nginx-lua-prometheus:."
+
+    set -eux; \
+        for component in ${lua_components}; do \
+            repo=$(echo "${component}" | cut -d: -f1); \
+            path=$(echo "${component}" | cut -d: -f2); \
+            dir_name=$(basename "${repo}"); \
+            \
+            echo "Installing ${repo}..."; \
+            rm -rf "${dir_name}"; \
+            git clone --depth 1 "https://github.com/${repo}" "${dir_name}"; \
+            \
+            cd "${dir_name}"; \
+            if [ -f Makefile ]; then \
+                make && \
+                make install \
+                    LUA_LIB_DIR="/usr/local/share/lua/5.1" \
+                    PREFIX="/usr/local"; \
+            else \
+                case "${repo}" in \
+                    cloudflare/lua-resty-logger-socket) \
+                        cp -R lib/resty/logger /usr/local/share/lua/5.1/resty/ ;; \
+                    knyar/nginx-lua-prometheus) \
+                        cp -v *.lua /usr/local/share/lua/5.1/ ;; \
+                    *) echo "Unknown component: ${repo}"; exit 1 ;; \
+                esac \
+            fi; \
+            cd ..; \
+            rm -rf "${dir_name}"; \
+        done
+
+    ls -l ${LUA_LIB_DIR}/resty
+    ls -l ${PREFIX}/lib/lua/${VER_LUA}
 
     echo "$VER_LUA" | tee /usr/local/lua_version
     echo "$ULTIMATE_BAD_BOT_BLOCKER" | tee /usr/local/ultimate_bot
